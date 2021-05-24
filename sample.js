49d0d4209cb00aa31eb37bd5162ddbdc4994d73c
diannediamante <mddiamante@up.edu.ph>
2021-05-24 09:06:39 +0800

    Add add to cart feature

--- sample.js
+++ sample.js
@@ -34,129 +34,67 @@ let fries_extras = [
     {"name": "BFF Fries", "price": 134.00}
 ];
 
-let menu = [burgers, desserts_drinks, ala_carte, fries_extras];
-let label = ["Burgers", "Desserts & Drinks", "A la Carte", "Fries & Extras"];
+let menu = [burgers, desserts_drinks, ala_carte, fries_extras];                 //array of items
+let label = ["Burgers", "Desserts & Drinks", "A la Carte", "Fries & Extras"];   //array of menu categories
+let cart = [];  //array of items bought
 
-
-
-
-
-var container = document.getElementById("menu_cont");
-id_cnt = 0;
+//dynamically add checkboxes on the html
+var container = document.getElementById("menu_cont");   //get div id
 for (i = 0; i < menu.length; i++){
-    var lab = document.createElement("label");
-    var t = document.createTextNode(label[i]);
+    var lab = document.createElement("label");  //create label tag
+    var t = document.createTextNode(label[i]);  //create text
 
-    lab.setAttribute("class", "menu_lbl");
-    lab.appendChild(t);
-    container.appendChild(lab);
-    container.appendChild(document.createElement("br"));
+    lab.setAttribute("class", "menu_lbl");      //set class name of the label
+    lab.appendChild(t);                         //add text to label
+    container.appendChild(lab);                 //add label to div
+    container.appendChild(document.createElement("br"));    //add a new line
  
     for (j = 0; j < menu[i].length; j++){
-        //set id of each object
-        Object.defineProperty(menu[i][j], "id", {value: id_cnt})
-        // console.log(menu[i][j].id);
-        id_cnt++;
-
-
-        var x = document.createElement("input");
-        var lbl = document.createElement("label");
+        var x = document.createElement("input");    //create input tag
+        var lbl = document.createElement("label");  //create label tag
         
-        x.setAttribute("type", "checkbox");
-        x.setAttribute("class", "menu");
-        x.setAttribute("onclick", "checkItem(this)");
-        x.setAttribute("value", menu[i][j].id.toString());
+        x.setAttribute("type", "checkbox");         //set input type as checkbox
+        x.setAttribute("class", "menu");            //set class as menu
+        x.setAttribute("onclick", "checkItem()");   //add click event
+        x.setAttribute("name", menu[i][j].name);        //set name as the name of the item
+        x.setAttribute("value", menu[i][j].price.toFixed(2).toString());    //set value as the price of the item
        
-        container.appendChild(x);
-        lbl.appendChild(document.createTextNode(menu[i][j].name));
-        lbl.appendChild(document.createElement("br"));
+        container.appendChild(x);       //add checkbox to div, there' no label yet
+        lbl.appendChild(document.createTextNode(x.name));   //add lable to checkbox
+        lbl.appendChild(document.createElement("br"));      //add new line
 
-        var price = document.createElement("label");
-        var price_txt = document.createTextNode("Php " + menu[i][j].price.toFixed(2).toString());
-        price.setAttribute("class", "price_lbl");
-        price.appendChild(price_txt);
+        var price = document.createElement("label");        //add label tag
+        var price_txt = document.createTextNode("Php " + x.value);  //add text
+        price.setAttribute("class", "price_lbl");           //set class name to "price_lbl"
+        price.appendChild(price_txt);                       //add price label
     
-        container.appendChild(lbl);
-        container.appendChild(price);
-        container.appendChild(document.createElement("br"));
-        container.appendChild(document.createElement("br"));
+        container.appendChild(lbl);                         //add item name to div
+        container.appendChild(price);                       //add item price ot div
+        container.appendChild(document.createElement("br"));    //add new line
+        container.appendChild(document.createElement("br"));    //add new line
     }
-    container.appendChild(document.createElement("br"));
+    container.appendChild(document.createElement("br"));    //add new line
 }
 
-// $("input:checkbox").each(function (){
-//     console.log(this.value);
-// });
+function checkItems(){      //check selected items
+    var elem = document.getElementsByClassName("menu");     //get the list of the elements with class name "menu"
+    var amount = 0.00;      //initial total amount
 
-function getPrice(id_num){
-    for (k = 0; k < menu.length; k++){
-        for (l = 0; l < menu[k].length; l++){
-            // console.log("i: " + i.toString() + " j: " + j.toString())
-            if (menu[k][l].id == Number(id_num)){
-                // console.log("found!");
-                // console.log(menu[k][l].name);
-                // break;
-                // return menu[k][l].price;
-                console.log(menu[k][l].price);
-                break;
-            }
-        }
-    }
-}
-
-function checkTotal(){
-    var elem = document.getElementsByClassName("menu");
-    var amount = 0;
+    cart = [];      //refresh/empty cart
     for (k = 0; k < elem.length; k++){
         if (elem.item(k).checked == true){
-            console.log(elem.item(k).value);
-            amount = amount + getPrice(elem.item(k).value);
+            //add to cart
+            cart.push({"name":elem.item(k).name, "price":(parseFloat(elem.item(k).value)).toFixed(2)});
+
+            //update total amount
+            amount = (parseFloat(amount) + parseFloat(elem.item(k).value)).toFixed(2);
         }
     }
-    console.log("total: " + amount.toString());
+    return amount;  //return the total amount to the function who called checkItems()
 }
 
-function checkItem(param){
-    // console.log(param.value);
-    for (k = 0; k < menu.length; k++){
-        for (l = 0; l < menu[k].length; l++){
-            // console.log("i: " + i.toString() + " j: " + j.toString())
-            if (menu[k][l].id == Number(param.value)){
-                // console.log("found!");
-                // console.log(menu[k][l].name);
-                break;
-            }
-        }
-    }
-    //add to receipt
-    //add total sum
-    checkTotal();
+function checkItem(){   //update cart and total amount
+    //get total sum
+    console.log("Total: " + checkItems().toString());
+    console.log(cart);
 }
-
-// function tryClick(){
-//     console.log("try!");
-// }
-// console.log(Number(document.getElementById("menu").value) + 10);
-
-// console.log(menu[3][0].name);
-// console.log(menu[3].length);
-
-
-// function load_menu(){
-
-// }
-
-// function check_burgers(){
-
-// }
-
-// function check_desserts_drinks(){
-    
-// }
-
-// function check_ala_carte(){
-    
-// }
-// function check_fries_extras(){
-    
-// }
